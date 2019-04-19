e38827bdf205ff71b0d00f3347810b0303592744
Carter <cartergreen5@outlook.com>
2019-04-19 13:50:13 -0700

    Updated disclaimer

--- Scripts/KFM_Deployment.ps1
+++ Scripts/KFM_Deployment.ps1
@@ -1,11 +1,23 @@
-# This Sample Code is provided for the purpose of illustration only and is not intended to be used in a production environment.
-# THIS SAMPLE CODE AND ANY RELATED INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED,
-# INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
-# We grant You a nonexclusive, royalty-free right to use and modify the Sample Code and to reproduce and distribute the object
-# code form of the Sample Code, provided that You agree: (i) to not use Our name, logo, or trademarks to market Your software
-# product in which the Sample Code is embedded; (ii) to include a valid copyright notice on Your software product in which the
-# Sample Code is embedded; and (iii) to indemnify, hold harmless, and defend Us and Our suppliers from and against any claims
-# or lawsuits, including attorneys’ fees, that arise or result from the use or distribution of the Sample Code.inst
+<#       
+    .DESCRIPTION
+        Script to enumerate OneDrive for Business Sites along with their data usage and date created. 
+        The sample scripts are not supported under any Microsoft standard support 
+        program or service. The sample scripts are provided AS IS without warranty  
+        of any kind. Microsoft further disclaims all implied warranties including,  
+        without limitation, any implied warranties of merchantability or of fitness for 
+        a particular purpose. The entire risk arising out of the use or performance of  
+        the sample scripts and documentation remains with you. In no event shall 
+        Microsoft, its authors, or anyone else involved in the creation, production, or 
+        delivery of the scripts be liable for any damages whatsoever (including, 
+        without limitation, damages for loss of business profits, business interruption, 
+        loss of business information, or other pecuniary loss) arising out of the use 
+        of or inability to use the sample scripts or documentation, even if Microsoft 
+        has been advised of the possibility of such damages.
+        
+        Author: Carter Green - cagreen@microsoft.com
+        
+        Deployment Guidnace: https://docs.microsoft.com/en-us/onedrive/redirect-known-folders        
+#>
 
 #CODE STARTS HERE
 #--TODO: Put your Tenant ID here, similar to $GivenTenantID =  'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx'
